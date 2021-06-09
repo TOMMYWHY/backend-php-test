216f1a07b61459d4468a96d0e57e4caf48fd316c
tommy <tommywhy1989@gmail.com>
2021-06-09 20:09:05 +1200

    TASK-{1}: {description required}

--- src/controllers.php
+++ src/controllers.php
@@ -72,11 +72,15 @@ $app->post('/todo/add', function (Request $request) use ($app) {
 
     $user_id = $user['id'];
     $description = $request->get('description');
+	if($description == null){
+		return $app->redirect('/todo');
+	}else{
+		$sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
+		$app['db']->executeUpdate($sql);
 
-    $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
-    $app['db']->executeUpdate($sql);
+		return $app->redirect('/todo');
+	}
 
-    return $app->redirect('/todo');
 });
 
 
